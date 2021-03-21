20b362103a4376c83f64805e2b4cb42aa439ef4b
Michael Ruhnau <michael@mruhnau.net>
2021-03-21 15:44:35 +0100

    add a controller

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,3 @@
 import dotenv from 'dotenv';
 dotenv.config();
-const var1 = process.env.TEST_ENV_VARIABLE;
-console.log(var1);
-console.log(process.env.TEST_ENV_VARIABLE);
-export const testEnvironmentVariable = var1;
+export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
