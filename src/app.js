df4dd8339a37d19e43b7b8d762d137426295649d
Michael Ruhnau <michael@mruhnau.net>
2021-03-22 17:52:42 +0100

    add messages stuff

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index';
 
 const app = express();
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,6 @@
 import dotenv from 'dotenv';
+
 dotenv.config();
 export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
+console.log(process.env.CONNECTION_STRING);
+export const connectionString = process.env.CONNECTION_STRING;
